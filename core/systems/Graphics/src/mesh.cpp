172a8c4e473acab32a10ef76d91598b62bfdc8ac
Allan Deutsch <allan@pwnolo.gy>
2015-03-18 04:00:30 -0700

    Fixed the missing pi define issue.

--- core/systems/Graphics/src/mesh.cpp
+++ core/systems/Graphics/src/mesh.cpp
@@ -45,7 +45,7 @@ namespace AlJeEngine
       glm::vec3 circleMesh[30000];
       for (int i = 0; i < 30000; ++i)
       {
-        float angle = 2.0f * __PI__ * ((float)i / 30000.0f);
+        float angle = 2.0f * 3.1415926535 * ((float)i / 30000.0f);
         circleMesh[i].x = cos(angle);
         circleMesh[i].y = sin(angle);
       }
